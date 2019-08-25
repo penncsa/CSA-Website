eca02f204b4bb784c443d6c80e181bd660d78f64
samuelxu <samuelgxu@gmail.com>
2019-08-24 23:55:13 -0400

    added www to CNAME

---
+++ build/precache-manifest.d83d09ce38279948c49919ff0cf23478.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed759955f398fd2859a3874cf15e7b0b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53bd67e578601f8bc1a3",
+    "url": "/static/css/main.da2178c4.chunk.css"
+  },
+  {
+    "revision": "eda848e432d52209e332",
+    "url": "/static/js/2.f26e4a07.chunk.js"
+  },
+  {
+    "revision": "53bd67e578601f8bc1a3",
+    "url": "/static/js/main.3d13bde3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "17c573ea5db86a6649e20f0e28cfcd10",
+    "url": "/static/media/board.17c573ea.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c95c962cb16aea84ddbcdfaba169beaf",
+    "url": "/static/media/penncsa.c95c962c.png"
+  }
+]);
